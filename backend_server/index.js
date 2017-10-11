8b6843a00cc9f5d53456ef0a4b473979c2442ee8
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2017-10-11 11:40:13 +0530

    procfile added, PORT vairable used, ready to deploy

--- backend_server/index.js
+++ backend_server/index.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
 var cors = require('cors');
+var path = require('path');
 var bodyParser = require('body-parser');
+
+var PORT = process.env.PORT || 9000;
 
 // Applying middleware
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static('./static')); //The static files HTML, CSS, JS are stored in this folder for serving the clients
 
 // Handling get request
 app.get('/testapiendpoint/get/', function(req, res) {
@@ -20,8 +24,17 @@ app.post('/testapiendpoint/post/', function(req, res) {
   res.send('Your POST request was successful. This is the response from the server');
 });
 
-var server = app.listen(9000, function() {
-  var address = server.address().address;
+// Assingment requests
+app.get('/assignment/get/', function(req, res) {
+  res.send('Hello there, your GET request was successful-- Assignment 1');
+});
+
+app.post('/assignment/post/', function(req, res) {
+  res.send('Hello there, your POST request was successful-- Assignment 2');
+});
+
+var server = app.listen(PORT, function() {
+  // var address = server.address().address;
   var port = server.address().port;
-  console.log('Server listening on ' + address + ':' + port);
+  console.log('Server listening on ' + port);
 });
